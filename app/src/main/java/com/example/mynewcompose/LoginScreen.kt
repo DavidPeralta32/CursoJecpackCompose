71152da5a71b66cad4bfd4ab66c8e2c144fb9655
David peralta <programador1@duxon.com.mx>
2024-04-20 09:55:50 -0600

    subo app cursdo

---
+++ app/src/main/java/com/example/mynewcompose/LoginScreen.kt
@@ -0,0 +1,7 @@
+package com.example.mynewcompose
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
---
+++ app/src/main/java/com/example/mynewcompose/MainActivity.kt
@@ -0,0 +1,92 @@
+package com.example.mynewcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.mynewcompose.ui.theme.MyNewComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyNewComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MyBox()
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun MyBox() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
+        Box(modifier = Modifier
+            .width(200.dp)
+            .height(200.dp)
+            .background(Color.Cyan)
+            .verticalScroll(
+                rememberScrollState()
+            ), contentAlignment = Alignment.Center
+        ){
+            Text("esto es un ejemplo")
+        }
+    }
+}
+
+@Composable
+fun MyColum(){
+    Column(
+        Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()), verticalArrangement = Arrangement.SpaceBetween) {
+        Text("ejemplo 1", modifier = Modifier
+            .background(Color.Red)
+            .fillMaxWidth())
+        Text("ejemplo 2", modifier = Modifier.background(Color.Black))
+        Text("ejemplo 3", modifier = Modifier.background(Color.Cyan))
+        Text("ejemplo 4", modifier = Modifier.background(Color.Yellow))
+    }
+
+}
+
+@Composable
+fun MiCaja2(mensaje:String){
+    Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center){
+        Box(modifier = Modifier.background(Color.Red)){
+            Text("${mensaje} esta es mi segunda caja")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MyNewComposeTheme {
+        MyColum()
+    }
+}
