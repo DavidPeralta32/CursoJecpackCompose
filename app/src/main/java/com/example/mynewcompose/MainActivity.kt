423acfee66032de0ee427dfce5dbe42a9182dfd0
David peralta <programador1@duxon.com.mx>
2024-04-20 10:24:16 -0600

    termino clases colunm, box y row

--- app/src/main/java/com/example/mynewcompose/MainActivity.kt
+++ app/src/main/java/com/example/mynewcompose/MainActivity.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -75,6 +77,24 @@ fun MyColum(){
 }
 
 @Composable
+fun MyRow(){
+    Row(
+        Modifier
+            .fillMaxSize()
+            .horizontalScroll(rememberScrollState()))
+    {
+        Text(text = "Holaa 1", modifier = Modifier.width(100.dp))
+        Text(text = "Holaa 2", modifier = Modifier.width(100.dp))
+        Text(text = "Holaa 3", modifier = Modifier.width(100.dp))
+        Text(text = "Holaa 4", modifier = Modifier.width(100.dp))
+        Text(text = "Holaa 5", modifier = Modifier.width(100.dp))
+        Text(text = "Holaa 6", modifier = Modifier.width(100.dp))
+    }
+}
+
+
+
+@Composable
 fun MiCaja2(mensaje:String){
     Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.Center){
         Box(modifier = Modifier.background(Color.Red)){
@@ -87,6 +107,6 @@ fun MiCaja2(mensaje:String){
 @Composable
 fun GreetingPreview() {
     MyNewComposeTheme {
-        MyColum()
+        MyRow()
     }
 }
